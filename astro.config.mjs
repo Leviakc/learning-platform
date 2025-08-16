ec61b527746767bb0cb1bcc1ff32016b75b9c53a
Juan gomen <gomen98.jg@gmail.com>
2025-08-15 23:42:45 -0600

    chore: add astro-icon and relative import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-
 import solidJs from "@astrojs/solid-js";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   },
 
-  integrations: [solidJs()],
+  integrations: [solidJs(), icon()],
 });
